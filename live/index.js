cd153abba220419d3819b2c18b871e4d712f927c
Neciu Dan <neciudan@Necius-MacBook-Pro.local>
2017-04-11 11:04:38 +0300

    made the app public

---
+++ live/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var compression = require('compression');
+
+var app = express();
+
+var oneDay = 8640000000;
+
+app.use(compression());
+
+app.use(express.static(__dirname + '/public', {maxAge: oneDay}));
+
+app.listen(5000)
